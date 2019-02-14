8ad7a1a1e1791976ea0a10d3b680cb5ab7aae1ec
Renkosky <philsky28@gmail.com>
2019-02-14 18:56:34 +0800

    post增加分页

--- routes/posts.js
+++ routes/posts.js
@@ -1,43 +1,45 @@
 const router = require("koa-router")();
-const Post = require('../db/models/post')
+const Post = require("../db/models/post");
 
 router.prefix("/api");
 
-router.post('/newPost',async ctx =>{
-    const { uid, author,title,content} = ctx.request.body;
-    let newpost = await Post.create({
-        uid, 
-        author, 
-        title, 
-        content
-    })
+router.post("/newPost", async ctx => {
+  const { uid, author, title, content } = ctx.request.body;
+  let newpost = await Post.create({
+    uid,
+    author,
+    title,
+    content
+  });
   if (newpost) {
-      ctx.body = {
-        code:0,
-        message:'发布成功'
-      }
-    }else{
-      ctx.body = {
-        code: -1,
-        message: "发布失败"
-      };
-    }
-})
-
-router.get("/getAllPosts", async ctx => {
-  const posts = await Post.find({});
-  if (posts) {
     ctx.body = {
-      code:0,
-      posts
+      code: 0,
+      message: "发布成功"
     };
-  }else{
-    ctx.body={
-      code: -1
-    }
-   
+  } else {
+    ctx.body = {
+      code: -1,
+      message: "发布失败"
+    };
   }
-  
 });
 
-module.exports = router
+router.post("/getAllPosts", async ctx => {
+  let { page } = ctx.request.body;
+  page = page || 1;
+  const posts = await Post.find({})
+    .skip((page - 1) * 8)
+    .limit(10);
+  if (posts) {
+    ctx.body = {
+      code: 0,
+      posts
+    };
+  } else {
+    ctx.body = {
+      code: -1
+    };
+  }
+});
+
+module.exports = router;
